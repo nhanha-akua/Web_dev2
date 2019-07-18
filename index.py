4ec49ba5acf41c9ef07dc02afa68029ff6487007
Genevieve Kankam Mensah <akuanhanha@gmail.com>
2019-07-18 14:02:49 +0000

    just added

---
+++ index.py
@@ -0,0 +1,29 @@
+from flask import *
+import json
+import os
+app = Flask(__name__)
+items = []
+@app.route('/')
+def index():
+    return render_template('form.html',items=items)
+
+@app.route('/foo/<name>')
+def foo(name):
+    return render_template('index.html', to=name)
+    
+    
+@app.route('/add_todo')
+def add_todo():
+    item = request.args.get("item")
+    items.append(item)
+    return redirect('http://localhost:5000/',code=302)
+    
+
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0',port=port)
+
+
+
+
